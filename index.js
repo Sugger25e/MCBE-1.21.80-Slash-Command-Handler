05f3c01164cb18d26ea49700ef2659a758a8081f
Sugger <87353568+Sugger25e@users.noreply.github.com>
2025-05-07 12:13:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,42 @@ import { slashRegistry } from "./slash/index.js"
 const COMMAND_PREFIX = "cmd:" //Required
 
 system.beforeEvents.startup.subscribe((init) => {
-  
   for (const cmd of slashRegistry) {
     const def = {
       ...cmd.data,
-      name: `${COMMAND_PREFIX}${cmd.data.name}`
+      name: `${COMMAND_PREFIX}${cmd.data.name}`,
     };
-    
-    init.customCommandRegistry.registerCommand(def, cmd.run);
+
+    init.customCommandRegistry.registerCommand(def, (origin, ...args) => {
+     let source;
+     switch(origin.sourceType) {
+       case "Entity":
+         source = {
+           ...origin.sourceEntity,
+           sourceType: origin.sourceType
+           };
+         break;
+       case "Block":
+         source = {
+           ...origin.sourceBlock,
+           sourceType: origin.sourceType
+           }
+         break;
+       case "NPCDialogue":
+         source = {
+           ...origin.sourceInitiator,
+           sourceType: origin.sourceType
+           }
+         break;
+       case "Server": 
+         source = {
+           ...origin.sourceEntity,
+           sourceType: origin.sourceType
+           }
+         break;
+     }
+
+      return cmd.run(system, source, args);
+    });
   }
 });
