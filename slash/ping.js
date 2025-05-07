3d1afcc5e5c590a7f9e8be6e358b3cb20243955f
Sugger <87353568+Sugger25e@users.noreply.github.com>
2025-05-07 13:19:47 +0800

    Update ping.js

--- slash/ping.js
+++ slash/ping.js
@@ -5,15 +5,12 @@ export default {
   data: new Command()
   .setName("ping")
   .setDescription("Ping pong")
-  .addStringOption("name", true) //option_name, required: true/false
+  .addStringOption("name", true)
   .addIntegerOption("amount", false)
   .setPermission(Permission.Any),
   run: (system, origin, args) => {
-  //when adding options, it must be in a correct sequence for args to be accurately working. 
-  // suggestion: add mandatory options first before optional ones.
-    
-    console.warn(JSON.stringify(origin.name))
-    world.sendMessage(`Hello, ${origin.sourceType === "Entity" ? origin.name : origin.sourceType}!`);
+  
+    world.sendMessage(`Hello, ${origin.sourceType === "Entity" ? origin.source.name : origin.sourceType}!`);
 
       world.sendMessage(`another ${args[0]}`)
     
@@ -21,6 +18,6 @@ export default {
       world.sendMessage(`yo ${args[1]}`)
     }
     
-    return { status: 0 }; 
+    return { status: 0 }; //required
   }
 };
